26aded18eb7e800e2ecf2337aeec27655e9ca8d6
wh201906 <wh201906@yandex.com>
2020-12-31 21:53:22 +0800

    ESP8266: nothing

--- Module/ESP8266/esp_arduino/esp_arduino.ino
+++ Module/ESP8266/esp_arduino/esp_arduino.ino
@@ -3,11 +3,15 @@
 
 #define RETRY_TIME 5
 
-#define TYPE_CLIENT 0
-#define TYPE_SERVER 1
+#define NETTYPE_TCPCLIENT 0
+#define NETTYPE_TCPSERVER 1
+#define NETTYPE_UDP 2
+
+#define DCPIN_DATA HIGH
+#define DCPIN_CMD LOW
 
 const uint8_t ledPin = 0;
-const uint8_t dataCmdPin = 2;
+const uint8_t dataCmdPin = 2; //HIGH is data, LOW is cmd
 const uint8_t txPin = 1;
 const uint8_t rxPin = 3;
 
@@ -16,15 +20,17 @@ char conf_ssid[32] = "TP-LINK";
 char conf_passwd[32] = "12345678";
 char conf_targetIP[16] = "192.168.1.100";
 uint16_t conf_port = 8266;
-uint8_t conf_type = TYPE_CLIENT;
+uint8_t conf_netType = NETTYPE_TCPCLIENT;
 
 SoftwareSerial mySerial(rxPin, txPin);
 WiFiClient myClient;
 WiFiServer myServer(conf_port);
 
+String cmdBuffer;
+
 void setup()
 {
-  pinMode(dataCmdPin, INPUT);
+  pinMode(dataCmdPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
 
   mySerial.begin(conf_baudrate);
@@ -32,10 +38,24 @@ void setup()
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(dataCmdPin) == DCPIN_DATA)
+  {
+    if (conf_netType == NETTYPE_TCPCLIENT)
+      while (mySerial.available())
+        myClient.write(mySerial.read());
+    else
+      while (mySerial.available())
+        myServer.write(mySerial.read());
+  }
+  else
+  {
+    if(!mySerial.available())
+    return;
+      cmdBuffer=mySerial.readString();
+  }
 }
 
-bool WifiInit()
+bool NetInit()
 {
   uint8_t i;
   WiFi.mode(WIFI_AP_STA);
@@ -48,7 +68,7 @@ bool WifiInit()
   }
   if (WiFi.status() != WL_CONNECTED)
     return false;
-  if (conf_type == TYPE_CLIENT)
+  if (conf_netType == NETTYPE_TCPCLIENT)
   {
     myClient.setNoDelay(true);
     myClient.connect(conf_targetIP, conf_port);
@@ -61,7 +81,7 @@ bool WifiInit()
     if (!myClient.connected())
       return false;
   }
-  else if (conf_type == TYPE_SERVER)
+  else if (conf_netType == NETTYPE_TCPSERVER)
   {
     myServer.setNoDelay(true);
     myServer.begin(conf_port);
