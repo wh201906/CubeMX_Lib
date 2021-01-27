af5c88970562dcb62335cc64134e1dd7179634d8
wh201906 <wh201906@yandex.com>
2021-01-27 18:07:40 +0800

    SoftI2C: Finish transmit logic for one byte

---
+++ Module/I2C/softi2c1.c
@@ -0,0 +1,114 @@
+#include "softi2c1.h"
+
+uint16_t delayTicks = 0;
+
+void SoftI2C1_Init(uint32_t speed)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  SOFTI2C1_SCL_CLKEN();
+  SOFTI2C1_SDA_CLKEN();
+
+  SOFTI2C1_SCL(1);
+  SOFTI2C1_SDA(1);
+  GPIO_InitStruct.Pin = SOFTI2C1_SCL_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(SOFTI2C1_SCL_GPIO, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = SOFTI2C1_SDA_PIN;
+  HAL_GPIO_Init(SOFTI2C1_SDA_GPIO, &GPIO_InitStruct);
+
+  delayTicks = Delay_GetSYSFreq() / speed;
+}
+
+void SoftI2C1_Start(void)
+{
+  SOFTI2C1_SDA_OUT();
+  SOFTI2C1_SCL(1);
+  SOFTI2C1_SDA(1);
+  Delay_ticks(delayTicks); // setup time
+  SOFTI2C1_SDA(0);         // START: when CLK is high,DATA change form HIGH to LOW
+  Delay_ticks(delayTicks); // hold time
+
+  SOFTI2C1_SCL(0); // cannot be read
+}
+
+void SoftI2C1_Stop(void)
+{
+  SOFTI2C1_SDA_OUT();
+  SOFTI2C1_SCL(1);
+  SOFTI2C1_SDA(0);
+  Delay_ticks(delayTicks);     // setup time
+  SOFTI2C1_SDA(1);             // STOP: when CLK is high,DATA change form LOW to HIGH
+  Delay_ticks(delayTicks * 2); // hold time(not necessary in most of the situations) and buff time(necessary)
+
+  // when the transmition is stopped, the SCL should be high
+}
+
+void SoftI2C1_SendACK(uint8_t isACK) // 1:ACK 0:NACK
+{
+  SOFTI2C1_SCL(0); // change start
+  SOFTI2C1_SDA_OUT();
+  SOFTI2C1_SDA(!isACK);        // 1:ACK 0:NACK
+  Delay_ticks(delayTicks / 4); // data setup time
+  SOFTI2C1_SCL(1);             // can be read
+  Delay_ticks(delayTicks);     // hold
+  SOFTI2C1_SCL(0);             // cannot be read
+  Delay_ticks(delayTicks / 4); // data setup time
+}
+
+uint8_t SoftI2C1_WaitACK(void) // 1:ACK 0:NACK/No response
+{
+  uint16_t waitTime = 0;
+  SOFTI2C1_SDA_IN();
+  SOFTI2C1_SCL(1);
+  delay_us(1);
+  while (SOFTI2C1_READSDA())
+  {
+    __NOP();
+    waitTime++;
+    if (waitTime > delayTicks)
+    {
+      SoftI2C1_Stop();
+      return 0;
+    }
+  }
+  SOFTI2C1_SCL(0);
+  return 1;
+}
+
+void SoftI2C1_SendByte(u8 byte)
+{
+  int8_t i;
+  SOFTI2C1_SDA_OUT();
+  SOFTI2C1_SCL(0);
+  for (t = 7; t >= 0; t--)
+  {
+    SOFTI2C1_SDA((byte >> t) & 1u);
+    Delay_ticks(delayTicks / 4); // data setup time
+    SOFTI2C1_SCL(1);
+    Delay_ticks(delayTicks); // data hold time
+    SOFTI2C1_SCL(0);
+    Delay_ticks(delayTicks / 4); // data setup time
+  }
+}
+
+uint8_t SoftI2C1_ReadByte(uint8_t sendACK) // 1: send ACK 0: no ACK
+{
+  uint8_t i, result = 0;
+  SOFTI2C1_SDA_IN();
+  for (i = 0; i < 8; i++)
+  {
+    IIC_SCL = 0;
+    Delay_ticks(delayTicks / 4); // data setup time
+    IIC_SCL = 1;
+    result <<= 1;
+    Delay_ticks(delayTicks / 4); // data setup time
+    result |= SOFTI2C1_READSDA() & 1u;
+    Delay_ticks(delayTicks); // data hold time
+  }
+  SoftI2C1_SendACK(sendACK);
+  return result;
+}
---
+++ Module/I2C/softi2c1.h
@@ -0,0 +1,44 @@
+#ifndef _SOFTI2C1_H
+#define _SOFTI2C1_H
+
+#include "main.h"
+#include "DELAY/delay.h"
+
+// ****** configuration start ******
+#define SOFTI2C1_SCL_CLKEN() __HAL_RCC_GPIOB_CLK_ENABLE()
+#define SOFTI2C1_SDA_CLKEN() __HAL_RCC_GPIOB_CLK_ENABLE()
+
+#define SOFTI2C1_SCL_GPIO GPIOB
+#define SOFTI2C1_SDA_GPIO GPIOB
+
+#define SOFTI2C1_SCL_PINID 8
+#define SOFTI2C1_SDA_PINID 9
+// ****** configuration end ******
+
+#define SOFTI2C1_SCL_PIN ((uint16_t)1u << SOFTI2C1_SCL_PINID)
+#define SOFTI2C1_SDA_PIN ((uint16_t)1u << SOFTI2C1_SDA_PINID)
+
+#define SOFTI2C1_SCL(__PINSTATE__) (SOFTI2C1_SCL_GPIO->BSRR = (uint32_t)SOFTI2C1_SCL_PIN << ((__PINSTATE__) ? (0u) : (16u)))
+#define SOFTI2C1_SDA(__PINSTATE__) (SOFTI2C1_SDA_GPIO->BSRR = (uint32_t)SOFTI2C1_SDA_PIN << ((__PINSTATE__) ? (0u) : (16u)))
+#define SOFTI2C1_READSDA() ((SOFTI2C1_SDA_GPIO->IDR >> SOFTI2C1_SDA_PINID) & 1u)
+
+#define SOFTI2C1_SDA_IN() {SOFTI2C1_SDA_GPIO->MODER &= ~(3 << (SOFTI2C1_SDA_PINID * 2u)); SOFTI2C1_SDA_GPIO->MODER |= 0 << (SOFTI2C1_SDA_PINID * 2u);}
+#define SOFTI2C1_SDA_OUT() {SOFTI2C1_SDA_GPIO->MODER &= ~(3 << (SOFTI2C1_SDA_PINID * 2u)); SOFTI2C1_SDA_GPIO->MODER |= 1 << (SOFTI2C1_SDA_PINID * 2u);}
+
+
+void SoftI2C1_Init(uint32_t speed);
+
+void SoftI2C1_Master_Transmit(uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+void SoftI2C1_Master_Receive(uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+
+void SoftI2C1_Slave_Transmit(uint8_t *pData, uint16_t Size, uint32_t Timeout);
+void SoftI2C1_Slave_Receive(uint8_t *pData, uint16_t Size, uint32_t Timeout);
+
+void SoftI2C1_Write(uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+void SoftI2C1_Read(uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+
+void SoftI2C1_IsDeviceReady(uint16_t DevAddress, uint32_t Trials, uint32_t Timeout);
+
+void SoftI2C1_Start(void);
+
+#endif
