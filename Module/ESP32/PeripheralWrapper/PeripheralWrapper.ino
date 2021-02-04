4f1ded62b0de4d62b10b314e7a1765f88efe7c48
wh201906 <wh201906@yandex.com>
2021-02-04 23:58:19 +0800

    ArduinoComm: Some useless codes

--- Module/ESP32/PeripheralWrapper/PeripheralWrapper.ino
+++ Module/ESP32/PeripheralWrapper/PeripheralWrapper.ino
@@ -1,11 +1,38 @@
 #include <HardwareSerial.h>
+#include <driver/spi_slave.h>
+#include <driver/gpio.h>
 #include <Ticker.h>
 #include <Arduino.h>
 
+#define WRAPPER_SERIAL_BAUDRATE 2000000
+#define WRAPPER_RX_PIN 16
+#define WRAPPER_TX_PIN 17
+
+#define WRAPPER_SCLK_PIN 13
+#define WRAPPER_MOSI_PIN 12
+#define WRAPPER_MISO_PIN 14
+
 HardwareSerial comSerial(1);
 
-uint8_t syncVariable=0;
-uint8_t asyncVariable=0;
+spi_bus_config_t buscfg = {
+  .mosi_io_num = WRAPPER_MOSI_PIN,
+  .miso_io_num = WRAPPER_MISO_PIN,
+  .sclk_io_num = WRAPPER_SCLK_PIN,
+  .quadwp_io_num = -1,
+  .quadhd_io_num = -1,
+};
+
+spi_slave_interface_config_t slvcfg = {
+  .mode = 0,
+  .spics_io_num = -1,
+  .queue_size = 1,
+  .flags = 0,
+  .post_setup_cb = my_post_setup_cb,
+  .post_trans_cb = my_post_trans_cb
+};
+
+uint8_t syncVariable = 0;
+uint8_t asyncVariable = 0;
 uint8_t receiveBuffer[128];
 uint8_t receiveLen;
 
@@ -26,9 +53,9 @@ void sensorTickerTask()
 void init()
 {
   // baudrate, mode, RxPin, TxPin
-  comSerial.begin(2000000,SERIAL_8N1,16,17);
-  comTicker.attach_ms(500,comTickerTask);
-  sensorTicker.attach_ms(1500,sensorTickerTask);
+  comSerial.begin(WRAPPER_SERIAL_BAUDRATE, SERIAL_8N1, WRAPPER_RX_PIN, WRAPPER_TX_PIN);
+  comTicker.attach_ms(500, comTickerTask);
+  sensorTicker.attach_ms(1500, sensorTickerTask);
 }
 
 
@@ -41,10 +68,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(comSerial.available())
+  if (comSerial.available())
   {
-    receiveLen=comSerial.readBytes(receiveBuffer,128);
+    receiveLen = comSerial.readBytes(receiveBuffer, 128);
     Serial.println(receiveLen);
-    Serial.write(receiveBuffer,receiveLen);
+    Serial.write(receiveBuffer, receiveLen);
   }
 }
