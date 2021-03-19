e66c602c3396979d99c29223514176e65d0e82f1
wh201906 <wh201906@yandex.com>
2021-03-19 17:25:21 +0800

    RMS: Update CubeF4 Firmware from 1.25.2 to 1.26.0

--- Project/F407VE_8M_SigPara/Core/Src/tim.c
+++ Project/F407VE_8M_SigPara/Core/Src/tim.c
@@ -29,9 +29,17 @@ TIM_HandleTypeDef htim2;
 /* TIM2 init function */
 void MX_TIM2_Init(void)
 {
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
 
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -53,6 +61,9 @@ void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
 
 }
 
