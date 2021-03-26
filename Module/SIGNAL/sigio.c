6b85c35f7c4ea5b78ca6f2a270c4c81f0f98c3b6
wh201906 <wh201906@yandex.com>
2021-03-26 22:55:50 +0800

    FIR: Quad buffer, buf0 and buf2 bypassed, no phase jump
    the buf0 will be write only once, the buf2 will be empty, the data is floating in buf1 and buf3
    need further analysis

--- Module/SIGNAL/sigio.c
+++ Module/SIGNAL/sigio.c
@@ -91,24 +91,23 @@ void SigIO_Start(void *bufAddr, uint32_t partLen)
 void DMA1_Stream1_IRQHandler()
 {
   __HAL_DMA_CLEAR_FLAG(&SigIO_DMA_DAC, __HAL_DMA_GET_TC_FLAG_INDEX(&SigIO_DMA_DAC));
-  nextDACPtr++;
+  nextDACPtr+=2;
   nextDACPtr %= 4;
   // Switched from Memory1 to Memory0, Memory1 is accessible
   if ((SigIO_DMA_DAC.Instance->CR & DMA_SxCR_CT) == RESET)
     HAL_DMAEx_ChangeMemory(&SigIO_DMA_DAC, (uint32_t)(SigIO_BaseBufAddr + nextDACPtr * SigIO_PartLen), MEMORY1);
   else
     HAL_DMAEx_ChangeMemory(&SigIO_DMA_DAC, (uint32_t)(SigIO_BaseBufAddr + nextDACPtr * SigIO_PartLen), MEMORY0);
-  nextADCPtr++;
+}
+
+void DMA2_Stream0_IRQHandler()
+{
+  __HAL_DMA_CLEAR_FLAG(&SigIO_DMA_ADC, __HAL_DMA_GET_TC_FLAG_INDEX(&SigIO_DMA_ADC));
+    nextADCPtr+=2;
   nextADCPtr %= 4;
   // Switched from Memory1 to Memory0, Memory1 is accessible
   if ((SigIO_DMA_ADC.Instance->CR & DMA_SxCR_CT) == RESET)
     HAL_DMAEx_ChangeMemory(&SigIO_DMA_ADC, (uint32_t)(SigIO_BaseBufAddr + nextADCPtr * SigIO_PartLen), MEMORY1);
   else
     HAL_DMAEx_ChangeMemory(&SigIO_DMA_ADC, (uint32_t)(SigIO_BaseBufAddr + nextADCPtr * SigIO_PartLen), MEMORY0);
-}
-
-void DMA2_Stream0_IRQHandler()
-{
-  __HAL_DMA_CLEAR_FLAG(&SigIO_DMA_ADC, __HAL_DMA_GET_TC_FLAG_INDEX(&SigIO_DMA_ADC));
-  
 }
