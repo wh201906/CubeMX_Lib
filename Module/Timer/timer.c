134009ea5eac2ac347c34933e276aeda811c0479
wh201906 <wh201906@yandex.com>
2021-04-05 14:11:02 +0800

    Timer: more precise version

--- Module/Timer/timer.c
+++ Module/Timer/timer.c
@@ -1,17 +1,19 @@
 #include "timer.h"
 
-uint64_t overflowedTicks = 0;
+uint64_t Timer_ticks = 0;
 TIM_HandleTypeDef Timer_handle;
 
 void Timer_Init(void)
 {
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
 
-  __HAL_RCC_TIM5_CLK_ENABLE();
-  Timer_handle.Instance = TIM5;
+  TIMER_TIM_CLKEN();
+  Timer_handle.Instance = TIMER_TIM;
   Timer_handle.Init.Prescaler = 0;
   Timer_handle.Init.CounterMode = TIM_COUNTERMODE_UP;
-  Timer_handle.Init.Period = 0xFF;
+
+  // For 16-bit timer, the period will saturate automatically
+  Timer_handle.Init.Period = 0xFFFFFFFF;
   Timer_handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   Timer_handle.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   HAL_TIM_Base_Init(&Timer_handle);
@@ -21,14 +23,15 @@ void Timer_Init(void)
   // If any interrupt happens between Start() and Stop(),
   // make sure the interrupt have lower priority than this one
   // or the Period(arr) is big enough.
-  HAL_NVIC_SetPriority(TIM5_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(TIM5_IRQn);
+  HAL_NVIC_SetPriority(TIMER_IRQN, 0, 0);
+  HAL_NVIC_EnableIRQ(TIMER_IRQN);
 }
 
 void Timer_Start(void)
 {
-  overflowedTicks = 0;
+  Timer_ticks = 0;
   __HAL_TIM_SET_COUNTER(&Timer_handle, 0);
+  __HAL_TIM_CLEAR_IT(&Timer_handle, TIM_IT_UPDATE);
   __HAL_TIM_ENABLE_IT(&Timer_handle, TIM_IT_UPDATE);
   __HAL_TIM_ENABLE(&Timer_handle);
 }
@@ -39,12 +42,12 @@ uint64_t Timer_Stop(void)
   result = __HAL_TIM_GET_COUNTER(&Timer_handle);
   __HAL_TIM_DISABLE_IT(&Timer_handle, TIM_IT_UPDATE);
   __HAL_TIM_DISABLE(&Timer_handle);
-  result += __HAL_TIM_GET_AUTORELOAD(&Timer_handle) * overflowedTicks;
+  result += __HAL_TIM_GET_AUTORELOAD(&Timer_handle) * Timer_ticks;
   return result;
 }
 
-void TIM5_IRQHandler(void)
+void TIM1_BRK_TIM9_IRQHandler(void)
 {
   __HAL_TIM_CLEAR_IT(&Timer_handle, TIM_IT_UPDATE);
-  overflowedTicks++;
+  Timer_ticks++;
 }
--- Module/Timer/timer.h
+++ Module/Timer/timer.h
@@ -3,6 +3,15 @@
 
 #include "main.h"
 
+// For STM32F4:
+// Use TIM2/TIM5(32-bit, 84MHz max) for lower interrupt times
+// Use TIM1/TIM8,TIM9~TIM11(16-bit, 168MHz max) for better precision
+
+#define TIMER_TIM_CLKEN() __HAL_RCC_TIM9_CLK_ENABLE()
+#define TIMER_TIM TIM9
+#define TIMER_IRQN TIM1_BRK_TIM9_IRQn
+// Remember to change TIMx_IRQHandler()
+
 void Timer_Init(void);
 void Timer_Start(void);
 uint64_t Timer_Stop(void);
