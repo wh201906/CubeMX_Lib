1578c3b985546f3a3e29ec8dc3387ab45c59a1d7
wh201906 <wh201906@yandex.com>
2021-04-14 16:30:20 +0800

    Add ports

--- app.py
+++ app.py
@@ -4,13 +4,27 @@ import serial
 
 port = "COM3"
 baud = 115200
+portDict = {
+    'sw0': ['a', '0'],
+    'sw1': ['a', '1'],
+    'sw2': ['a', '2'],
+    'sw3': ['a', '3'],
+    'sw4': ['a', '4'],
+    'sw5': ['a', '5'],
+    'sw6': ['a', '6'],
+    'sw7': ['a', '7'],
+}
 app = Flask(__name__)
 serialInst = serial.Serial(port, baud)
 
 
 @app.route('/switch', methods=['POST'])
 def switch():
-    print(request.form['id'], request.form['state'])
+    id = str(request.form['id'])
+    st = str(request.form['state'])
+    print(id, st)
+    port = portDict[id]
+    cmd = 'w,' + port[0] + ',' + port[1] + ',' + '>'
     return 'button switched'
 
 
