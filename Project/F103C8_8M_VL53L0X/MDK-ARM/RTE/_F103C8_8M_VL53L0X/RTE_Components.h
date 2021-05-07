fc20d23fa7d3f715a405041d542cc155264285ff
wh201906 <wh201906@yandex.com>
2021-05-07 18:04:31 +0800

    VL53L0X: Add F103 project

---
+++ Project/F103C8_8M_VL53L0X/MDK-ARM/RTE/_F103C8_8M_VL53L0X/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'F103C8_8M_VL53L0X' 
+ * Target:  'F103C8_8M_VL53L0X' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
