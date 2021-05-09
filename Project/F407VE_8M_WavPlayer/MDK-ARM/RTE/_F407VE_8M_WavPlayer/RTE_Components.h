ac9122737d3eab05aaf599298036339c6abdea06
wh201906 <wh201906@yandex.com>
2021-05-09 16:50:53 +0800

    WavPlayer: Dump and useless
    the PCM5102A doesn't appear like a normal DAC
    the MX_SDIO_SD_Init() will call Error_Handler() if no card is detected

---
+++ Project/F407VE_8M_WavPlayer/MDK-ARM/RTE/_F407VE_8M_WavPlayer/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'F407VE_8M_WavPlayer' 
+ * Target:  'F407VE_8M_WavPlayer' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
