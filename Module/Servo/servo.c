00568347e508fc75a60e33ccb1b433cbd5b141f6
wh201906 <wh201906@yandex.com>
2021-07-05 15:28:21 +0800

    Servo: Finished

--- Module/Servo/servo.c
+++ Module/Servo/servo.c
@@ -4,7 +4,7 @@ void Servo_Init(ServoHandle *handle, TIM_HandleTypeDef *htim, uint32_t Channel, 
 {
   handle->htim = htim;
   handle->Channel = Channel;
-  handle->delay = 1000; // default delay
+  handle->delay = 500; // default delay
   handle->htim->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   handle->htim->Instance->CR1 |= TIM_CR1_ARPE;
   __HAL_TIM_SET_AUTORELOAD(handle->htim, 20000); // 20ms, 50Hz
@@ -13,40 +13,59 @@ void Servo_Init(ServoHandle *handle, TIM_HandleTypeDef *htim, uint32_t Channel, 
 
 void Servo_Start(ServoHandle *handle)
 {
-  uint32_t i, delayLen;
-  handle->htim->Instance->CR1 |= TIM_CR1_OPM;
-  delayLen = 1000000;
-  HAL_TIM_PWM_Start(handle->htim, handle->Channel);
-  for (i = 0; i * delayLen < 6000000; i++) // 3s
-  {
-    //HAL_TIM_PWM_Start(handle->htim, handle->Channel);
-    TIM_CHANNEL_STATE_SET(handle->htim, handle->Channel, HAL_TIM_CHANNEL_STATE_READY);
-    Delay_us(delayLen);
-  }
-  handle->htim->Instance->CR1 &= ~TIM_CR1_OPM;
   HAL_TIM_PWM_Start(handle->htim, handle->Channel);
 }
 
 void Servo_Go(ServoHandle *handle, uint16_t pos)
 {
-  uint16_t currPos;
+  int8_t i;
+  uint16_t startPulse, currPulse, endPulse;
+  uint16_t threP[3] = {10, 5, 2};
+  float threD[3] = {3.0, 2.0, 1.5};
 
-  pos = __HAL_TIM_GET_AUTORELOAD(handle->htim) - pos;
-  currPos = __HAL_TIM_GET_COMPARE(handle->htim, handle->Channel);
-  if (currPos < pos)
+  endPulse = __HAL_TIM_GET_AUTORELOAD(handle->htim) - pos;
+  startPulse = __HAL_TIM_GET_COMPARE(handle->htim, handle->Channel);
+  currPulse = startPulse;
+  if (currPulse < endPulse)
   {
-    for (; currPos <= pos; currPos++)
+    for (i = 0; i < 3; i++)
     {
-      Delay_us(handle->delay);
-      __HAL_TIM_SET_COMPARE(handle->htim, handle->Channel, currPos);
+      threP[i] = startPulse + (endPulse - startPulse) / threP[i];
+    }
+    for (; currPulse <= endPulse; currPulse++)
+    {
+      for (i = 2; i >= 0; i--)
+      {
+        if (currPulse <= threP[i])
+        {
+          Delay_us(handle->delay * threD[i]);
+          break;
+        }
+      }
+      if (i < 0)
+        Delay_us(handle->delay);
+      __HAL_TIM_SET_COMPARE(handle->htim, handle->Channel, currPulse);
     }
   }
   else
   {
-    for (; currPos >= pos; currPos--)
+    for (i = 0; i < 3; i++)
     {
-      Delay_us(handle->delay);
-      __HAL_TIM_SET_COMPARE(handle->htim, handle->Channel, currPos);
+      threP[i] = startPulse - (startPulse - endPulse) / threP[i];
+    }
+    for (; currPulse >= endPulse; currPulse--)
+    {
+      for (i = 2; i >= 0; i--)
+      {
+        if (currPulse >= threP[i])
+        {
+          Delay_us(handle->delay * threD[i]);
+          break;
+        }
+      }
+      if (i < 0)
+        Delay_us(handle->delay);
+      __HAL_TIM_SET_COMPARE(handle->htim, handle->Channel, currPulse);
     }
   }
 }
