abe038967e1ed2f2d61fae23b5ca931b14479152
wh201906 <wh201906@yandex.com>
2021-07-05 13:41:18 +0800

    Servo: TIM10 Test passed
    The TIM10 has one pulse mode, even the register map doesn't have OPM bit.

--- Project/F407VE_8M_Servo/Core/Inc/tim.h
+++ Project/F407VE_8M_Servo/Core/Inc/tim.h
@@ -31,14 +31,12 @@ extern "C" {
 
 /* USER CODE END Includes */
 
-extern TIM_HandleTypeDef htim9;
 extern TIM_HandleTypeDef htim10;
 
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
 
-void MX_TIM9_Init(void);
 void MX_TIM10_Init(void);
 
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
