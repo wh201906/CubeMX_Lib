8c78d4dd83f3045acbba989276416ec4055e2f9e
wh201906 <wh201906@yandex.com>
2021-08-02 21:38:13 +0800

    AD7606: macro for serial interface

---
+++ Module/AD7606/ad7606.c
@@ -0,0 +1,27 @@
+#include "ad7606.h"
+#include "DELAY/delay.h"
+
+void AD7606_Init(void)
+{
+
+}
+
+void AD7606_SetOversample(uint8_t oversample)
+{
+
+}
+
+uint16_t AD7606_GetVal(uint8_t id)
+{
+
+}
+
+void AD7606_StartConvA(void)
+{
+
+}
+
+void AD7606_StartConvB(void)
+{
+
+}
