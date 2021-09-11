7ffda96302f44126e112929d88ccd54f2859808b
wh201906 <wh201906@yandex.com>
2021-09-11 23:57:12 +0800

    WaveTransfer: Init
    Use different .uvprojx files for different nodes
    Use compile option -D to share the same set of code

---
+++ Project/F407VE_8M_WaveTransfer/Core/Inc/node.h
@@ -0,0 +1,24 @@
+#ifndef _NODE_H_
+#define _NODE_H_
+
+#include "main.h"
+#include "UART/myuart.h"
+#ifndef NODE_ID
+#define NODE_ID '0' // '0':server, '1':client1, '2':client2
+#endif
+
+extern uint64_t updateTimes;
+extern MyUARTHandle uart1, uart2;
+
+#define NODE_UART &uart2
+
+#define Node_getCurrTick() (updateTimes << 16 | TIM10->CNT)
+#define Node_Write(__STR__, __LEN__) MyUART_Write(NODE_UART, (__STR__), (__LEN__))
+#define Node_ReadUntil(__STR__, __ENDCHAR__) MyUART_ReadUntil(NODE_UART, (__STR__), (__ENDCHAR__))
+
+void Node_EventLoop(void);
+void Node_PacketHead(uint8_t target);
+uint64_t Node_GetDelay(uint8_t target);
+void Node_AckDelay(void);
+
+#endif
---
+++ Project/F407VE_8M_WaveTransfer/Core/Src/node.c
@@ -0,0 +1,49 @@
+#include "node.h"
+
+uint64_t updateTimes = 0;
+uint8_t nodeBuf[20]; // [0]:source [1]:target [2:]:payload
+
+void Node_PacketHead(uint8_t target)
+{
+  MyUART_WriteChar(NODE_UART, NODE_ID);
+  MyUART_WriteChar(NODE_UART, target);
+}
+
+void Node_EventLoop(void)
+{
+  if(Node_ReadUntil(nodeBuf, '>') && nodeBuf[1] == NODE_ID)
+  {
+    Node_AckDelay();
+  }
+}
+
+uint64_t Node_GetDelay(uint8_t target)
+{
+  uint64_t start;
+  start = Node_getCurrTick();
+  MyUART_ClearBuffer(NODE_UART);
+  Node_PacketHead(target);
+  Node_Write("1>", 2);
+  while(1)
+  {
+    if(!Node_ReadUntil(nodeBuf, '>'))
+      continue;
+    if(nodeBuf[0] == target && nodeBuf[1] == NODE_ID)
+      break;
+  }
+  return (Node_getCurrTick() - start);
+}
+
+void Node_AckDelay(void)
+{
+  if(nodeBuf[3] == '1')
+  {
+    Node_PacketHead(nodeBuf[0]);
+    Node_Write("1>", 2);
+  }
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  updateTimes++;
+}
