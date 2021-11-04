a6985d636f55c188169ef516ee887171284d2cd4
wh201906 <wh201906@yandex.com>
2021-11-04 16:50:54 +0800

    Mod: Tx

---
+++ Module/Mod/mod.c
@@ -0,0 +1,95 @@
+#include "mod.h"
+#include "tim.h"
+
+WS2812_Dev *WS2812_currDev;
+uint32_t WS2812_counter;
+uint32_t WS2812_buf[8];
+uint32_t WS2812_currTIMChannel;
+
+void WS2812_DMAInit(WS2812_Dev *dev, DMA_Stream_TypeDef *DMAStream, uint32_t DMAChannel, IRQn_Type DMAIRQ)
+{
+  if ((uint32_t)DMAStream > DMA2_BASE)
+    __HAL_RCC_DMA2_CLK_ENABLE();
+  else if ((uint32_t)DMAStream > DMA1_BASE)
+    __HAL_RCC_DMA1_CLK_ENABLE();
+
+  dev->DMAHandle.Instance = DMAStream;
+#if defined(STM32H750xx)
+  dev->DMAHandle.Init.Request = DMAChannel;
+#else
+  dev->DMAHandle.Init.Channel = DMAChannel;
+#endif
+  dev->DMAHandle.Init.Direction = DMA_MEMORY_TO_PERIPH;
+  dev->DMAHandle.Init.PeriphInc = DMA_PINC_DISABLE;
+  dev->DMAHandle.Init.MemInc = DMA_MINC_ENABLE;
+  dev->DMAHandle.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+  dev->DMAHandle.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+  dev->DMAHandle.Init.Mode = DMA_CIRCULAR;
+  dev->DMAHandle.Init.Priority = DMA_PRIORITY_VERY_HIGH;
+  dev->DMAHandle.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+  
+  HAL_DMA_DeInit(&dev->DMAHandle);
+  HAL_DMA_Init(&dev->DMAHandle);
+
+  __HAL_LINKDMA(dev->htim, hdma[TIM_DMA_ID_UPDATE], dev->DMAHandle);
+
+  HAL_NVIC_SetPriority(DMAIRQ, 1, 0);
+  HAL_NVIC_EnableIRQ(DMAIRQ);
+
+  __HAL_DMA_CLEAR_FLAG(&dev->DMAHandle, __HAL_DMA_GET_TC_FLAG_INDEX(&dev->DMAHandle));
+  __HAL_DMA_CLEAR_FLAG(&dev->DMAHandle, __HAL_DMA_GET_HT_FLAG_INDEX(&dev->DMAHandle));
+  ((DMA_Stream_TypeDef *)dev->DMAHandle.Instance)->CR |= DMA_IT_TC | DMA_IT_HT;
+}
+
+void WS2812_Init(WS2812_Dev *dev, DMA_Stream_TypeDef *DMAStream, uint32_t DMAChannel, IRQn_Type DMAIRQ, TIM_HandleTypeDef *htim)
+{
+  uint32_t tmp;
+  dev->htim = htim;
+
+  // for PWM mode 1
+  tmp = __HAL_TIM_GET_AUTORELOAD(htim);
+  dev->comp0 = tmp * 0.32;
+  dev->comp1 = tmp - dev->comp0;
+  WS2812_DMAInit(dev, DMAStream, DMAChannel, DMAIRQ);
+}
+
+void WS2812_Write(WS2812_Dev *dev, uint32_t TIMChannel, uint8_t *data, uint32_t len)
+{
+  uint8_t i;
+  WS2812_currDev = dev;
+  WS2812_currDev->data = data;
+  WS2812_currDev->len = len;
+  WS2812_currTIMChannel = TIMChannel;
+  WS2812_counter = 0;
+  for (i = 0; i < 8; i++)
+    WS2812_buf[i] = WS2812_currDev->comp0;
+  __HAL_TIM_SET_COMPARE(WS2812_currDev->htim, TIMChannel, WS2812_currDev->comp0);
+  WS2812_currDev->htim->Instance->EGR = TIM_EGR_UG; // force update
+  __HAL_TIM_SET_COUNTER(WS2812_currDev->htim, 0);
+  HAL_TIM_PWM_Start(WS2812_currDev->htim, TIMChannel);
+  HAL_DMA_Init(&WS2812_currDev->DMAHandle); // necessary there
+  __HAL_TIM_ENABLE_DMA(WS2812_currDev->htim, TIM_DMA_UPDATE);
+  HAL_DMA_Start(&WS2812_currDev->DMAHandle, (uint32_t)WS2812_buf, (uint32_t) & (WS2812_currDev->htim->Instance->CCR1) + TIMChannel, 8);
+  while (((DMA_Stream_TypeDef *)WS2812_currDev->DMAHandle.Instance)->CR & DMA_SxCR_EN)
+    ;
+}
+
+inline void WS2812_UpdateBuf(void)
+{
+  uint8_t i;
+  if (WS2812_counter >= (WS2812_currDev->len+1) * 8) // boundary condition, modify this if necessary
+  {
+    __HAL_DMA_DISABLE(&WS2812_currDev->DMAHandle);
+    // wait for the last bit
+    while(__HAL_TIM_GET_COUNTER(WS2812_currDev->htim) >= __HAL_TIM_GET_COMPARE(WS2812_currDev->htim, WS2812_currTIMChannel)) // boundary condition, modify this if necessary
+      ;
+    __HAL_TIM_SET_COMPARE(WS2812_currDev->htim, WS2812_currTIMChannel, WS2812_currDev->comp0);
+  }
+  i = 4;
+  while (i--)
+  {
+    WS2812_buf[WS2812_counter % 8] = WS2812_currDev->data[WS2812_counter / 8] << (WS2812_counter % 8) & 0x80 ? WS2812_currDev->comp1 : WS2812_currDev->comp0;
+    WS2812_counter++;
+  }
+  __HAL_DMA_CLEAR_FLAG(&WS2812_currDev->DMAHandle, __HAL_DMA_GET_HT_FLAG_INDEX(&WS2812_currDev->DMAHandle) | __HAL_DMA_GET_TC_FLAG_INDEX(&WS2812_currDev->DMAHandle));
+}
---
+++ Project/F407VE_8M_Rx/Core/Src/tim.c
@@ -0,0 +1,121 @@
+/**
+  ******************************************************************************
+  * @file    tim.c
+  * @brief   This file provides code for the configuration
+  *          of the TIM instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "tim.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+TIM_HandleTypeDef htim9;
+
+/* TIM9 init function */
+void MX_TIM9_Init(void)
+{
+
+  /* USER CODE BEGIN TIM9_Init 0 */
+
+  /* USER CODE END TIM9_Init 0 */
+
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM9_Init 1 */
+
+  /* USER CODE END TIM9_Init 1 */
+  htim9.Instance = TIM9;
+  htim9.Init.Prescaler = 0;
+  htim9.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim9.Init.Period = 65535;
+  htim9.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim9.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_IC_Init(&htim9) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim9, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM9_Init 2 */
+
+  /* USER CODE END TIM9_Init 2 */
+
+}
+
+void HAL_TIM_IC_MspInit(TIM_HandleTypeDef* tim_icHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(tim_icHandle->Instance==TIM9)
+  {
+  /* USER CODE BEGIN TIM9_MspInit 0 */
+
+  /* USER CODE END TIM9_MspInit 0 */
+    /* TIM9 clock enable */
+    __HAL_RCC_TIM9_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**TIM9 GPIO Configuration
+    PA2     ------> TIM9_CH1
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF3_TIM9;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN TIM9_MspInit 1 */
+
+  /* USER CODE END TIM9_MspInit 1 */
+  }
+}
+
+void HAL_TIM_IC_MspDeInit(TIM_HandleTypeDef* tim_icHandle)
+{
+
+  if(tim_icHandle->Instance==TIM9)
+  {
+  /* USER CODE BEGIN TIM9_MspDeInit 0 */
+
+  /* USER CODE END TIM9_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM9_CLK_DISABLE();
+
+    /**TIM9 GPIO Configuration
+    PA2     ------> TIM9_CH1
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2);
+
+  /* USER CODE BEGIN TIM9_MspDeInit 1 */
+
+  /* USER CODE END TIM9_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
